825b97c696ff63caabe3f71512be6d35960a04d7
JSRafael <josue_samuel_92@hotmail.com>
2014-05-10 18:10:30 -0500

    fondo principal
    corrigiendo el fondo para uno mejor, así como los estilos.

--- themes/nuevo/views/layouts/main.php
+++ themes/nuevo/views/layouts/main.php
@@ -23,35 +23,9 @@
 		<!--[if lte IE 8]><style>.support-note .note-ie{display:block;}</style><![endif]-->
 
 	</head>
-	<meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
-    <style type="text/css">
-      html { height: 100% }
-      body { height: 100%; margin: 0; padding: 0 }
-      #map_canvas { height: 100% }
-    </style>
-    <script src="https://maps.googleapis.com/maps/api/js?key=Jruy4OS73lkYu0BxDMLdGkxa&sensor=SET_TO_TRUE_OR_FALSE"
-  type="text/javascript"></script>
-    <script type="text/javascript">
-      function initialize() {
-  var mapOptions = {
-    zoom: 8,
-    center: new google.maps.LatLng(-34.397, 150.644),
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  }
-  var map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
-}
-
-function loadScript() {
-  var script = document.createElement("script");
-  script.type = "text/javascript";
-  script.src = "http://maps.googleapis.com/maps/api/js?key=Jruy4OS73lkYu0BxDMLdGkxa&sensor&sensor=TRUE&callback=initialize";
-  document.body.appendChild(script);
-}
-
-window.onload = loadScript;
-    </script>
-    <body onload="initialize()">
-    <div id="map_canvas" style="width:100%; height:100%"></div>
+	
+    <body>
+   
         <div class="container">
 		
 				 
